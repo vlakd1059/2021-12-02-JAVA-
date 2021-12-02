64696ccafa2a98fc765cb308e1add3ae591b1a1e
vlakd1059 <49181149+vlakd1059@users.noreply.github.com>
2021-12-02 09:02:00 +0900

    반복문 1~2:약수, 3:완전수 구하기

--- Exam01.java
+++ Exam01.java
@@ -1,22 +1,18 @@
-package 배열;
+import java.util.Scanner;
 
 public class Exam01 {
 
 	public static void main(String[] args) {
-		//배열 : 같은 타입의 여러 변수를 하나의 묶음으로 다루는것.
-		//배열의 길이 5인 정수형 배열 array 생성
-		int [] array= new int[5]; // 정수형 길이가 5인
-		array[0]=27;
-		array[1]=30;
-		array[2]=15;
-		array[3]=44;
-		array[4]=31;
-		System.out.println(array[0]);
-		System.out.println(array[1]);
-		System.out.println(array[2]);
-		System.out.println(array[3]);
-		System.out.println(array[4]);
-
+		Scanner sc = new Scanner(System.in);
+		System.out.print("정수입력 : ");
+		int num = sc.nextInt();
+		System.out.print(num + "의 약수 : ");
+		// for (int i = 1; i <= num / 2; i++)
+		for (int i = 1; i < num; i++) {
+			if (num % i == 0) {
+				System.out.print(i + ", ");
+			}
+		}
+		System.out.println(num);
 	}
-
 }
--- Exam03.java
+++ Exam03.java
@@ -1,14 +1,19 @@
-package 배열;
 
 public class Exam03 {
 
 	public static void main(String[] args) {
-		int[] array1 = { 10, 20, 30, 40, 50 };
-//		System.out.println("주소 1의 값 >> "+array1[1]);
-//		System.out.println("길이 >> "+array1.length);
-		int[] array2 = array1;
-		array1[3]=999;
-		System.out.println(array2[3]);
-
+		
+		// 1~1000 완전수
+		// 완전수: 약수중 자기 자신 수랑 똑같은값
+		for (int i = 1; i <= 1000; i++) {
+			int sum = 0;
+			for (int j = 1; j < i; j++) {
+				if (i % j == 0) {
+					sum = sum + j;
+				}
+			}
+			if (i == sum)
+				System.out.println(sum);
+		}
 	}
 }
